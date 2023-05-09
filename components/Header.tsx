33472d6ea948712ce9693143e066569f2400b308
iGEORGE <www.igeorge.www@gmail.com>
2023-05-09 18:42:38 +0100

    rkjlkekdrgrg

--- components/Header.tsx
+++ components/Header.tsx
@@ -11,7 +11,7 @@ const Header = () => {
                 <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
             </label>
             <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-                <li><Link href="#home">Home</Link></li>
+                <li><Link href="#hero">Home</Link></li>
                 <li><Link href="#about">About</Link></li>
                 <li><Link href="#projects">Projects</Link></li>
                 <li><Link href="#contact">Contact</Link></li>
@@ -21,7 +21,7 @@ const Header = () => {
         </div>
         <div className=" hidden lg:flex">
             <ul className="menu menu-horizontal px-1 font-medium">
-            <li><Link href="#home">Home</Link></li>
+            <li><Link href="#hero">Home</Link></li>
             <li><Link href="#about">About</Link></li>
             <li><Link href="#projects">Projects</Link></li>
             <li><Link href="#contact">Contact</Link></li>
